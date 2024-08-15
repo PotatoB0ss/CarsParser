b134bca3ba85b843e210bb6ca0ea1ea363db5149
Artsiom Starastsenka <drafasenos317@gmail.com>
2024-08-15 14:26:10 +0300

    Completed and fixed

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace CarsParser
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
